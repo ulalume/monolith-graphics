8ec306fe941be83a6acd154c8ea248b8db5d56ed
kbt <kubota@softdevice.co.jp>
2022-08-14 11:56:47 +0900

    bug fix

--- color.lua
+++ color.lua
@@ -1,11 +1,11 @@
 local color = {}
 color.__index = color
 
-function map(value, srcMin, srcMax, dstMin, dstMax)
+local function map(value, srcMin, srcMax, dstMin, dstMax)
   return dstMin + (dstMax - dstMin) * ((value - srcMin) / (srcMax - srcMin))
 end
 
-function fromHex(...)
+local function fromHex(...)
   local values = { ... }
   for i, v in ipairs(values) do
     values[i] = v / 0xff
@@ -13,7 +13,7 @@ function fromHex(...)
   return unpack(values)
 end
 
-function toHex(...)
+local function toHex(...)
   local values = { ... }
   for i, v in ipairs(values) do
     values[i] = math.floor(v * 0xff)
@@ -22,7 +22,7 @@ function toHex(...)
 end
 
 function color.new(r, g, b, a)
-  return setmetatable({r = r, g = g, b = b, a = a}, color)
+  return setmetatable({ r = r, g = g, b = b, a = a }, color)
 end
 
 function color.withRgb(r, g, b)
@@ -54,7 +54,7 @@ function color.lerp(a, b, t)
     a.r + (b.r - a.r) * t,
     a.g + (b.g - a.g) * t,
     a.b + (b.r - a.b) * t,
-  a.a + (b.a - a.a) * t)
+    a.a + (b.a - a.a) * t)
 end
 
 function color.gradient(posAndColors, numShades)
@@ -80,78 +80,77 @@ function color.gradient(posAndColors, numShades)
     -- find next color
     local thresh = map(pos, posAndColor0[1], posAndColor1[1], 0, 1)
     while thresh > 0.5
-      and index < #posAndColors do
+        and index < #posAndColors do
 
-        index = index + 1
-        posAndColor0 = posAndColor1
-        posAndColor1 = posAndColors[index]
-        thresh = map(pos, posAndColor0[1], posAndColor1[1], 0, 1)
-      end
+      index = index + 1
+      posAndColor0 = posAndColor1
+      posAndColor1 = posAndColors[index]
+      thresh = map(pos, posAndColor0[1], posAndColor1[1], 0, 1)
+    end
 
-      local t = map(pos, posAndColor0[1], posAndColor1[1], 0.0, 1.0)
-      local c = color.lerp(
-        posAndColor0[2],
-        posAndColor1[2],
+    local t = map(pos, posAndColor0[1], posAndColor1[1], 0.0, 1.0)
+    local c = color.lerp(
+      posAndColor0[2],
+      posAndColor1[2],
       t)
-      table.insert(result, c)
-    end
-    return result
+    table.insert(result, c)
   end
+  return result
+end
 
-  function color:__tostring()
-    return string.format(
-      "color { r:%.1f, g: %.1f, b: %.1f, a: %.1f }",
-      self.r,
-      self.g,
-      self.b,
+function color:__tostring()
+  return string.format(
+    "color { r:%.1f, g: %.1f, b: %.1f, a: %.1f }",
+    self.r,
+    self.g,
+    self.b,
     self.a)
-  end
+end
 
-  function color:rgb()
-    return self.r, self.g, self.b
-  end
+function color:rgb()
+  return self.r, self.g, self.b
+end
 
-  function color:rgbHex()
-    return toHex(self:rgb())
-  end
+function color:rgbHex()
+  return toHex(self:rgb())
+end
 
-  function color:rgba()
-    return self.r, self.g, self.b, self.a
-  end
+function color:rgba()
+  return self.r, self.g, self.b, self.a
+end
 
-  function color:rgbaHex()
-    return toHex(self:rgba())
-  end
+function color:rgbaHex()
+  return toHex(self:rgba())
+end
 
+function color.swapImageDataColor(srcImageData, targetColor, swapColor)
+  local dstImageData = srcImageData:clone()
+  local w, h = dstImageData:getDimensions()
 
-  function color.swapImageDataColor(srcImageData, targetColor, swapColor)
-    local dstImageData = srcImageData:clone()
-    local w, h = dstImageData:getDimensions()
-
-    for col = 1, w - 1 do
-      for row = 1, h - 1 do
-        local r, g, b, a = dstImageData:getPixel(col, row)
-        if r == targetColor.r
-        and g == targetColor.g
-        and b == targetColor.b
-        and a == targetColor.a then
-          dstImageData:setPixel(
-            col,
-            row,
+  for col = 1, w - 1 do
+    for row = 1, h - 1 do
+      local r, g, b, a = dstImageData:getPixel(col, row)
+      if r == targetColor.r
+          and g == targetColor.g
+          and b == targetColor.b
+          and a == targetColor.a then
+        dstImageData:setPixel(
+          col,
+          row,
           swapColor:rgba())
-        end
       end
     end
-    return dstImageData
   end
+  return dstImageData
+end
 
-  color.white = color.withRgb(1, 1, 1)
-  color.black = color.withRgb(0, 0, 0)
-  color.red = color.withRgb(1, 0, 0)
-  color.green = color.withRgb(0, 1, 0)
-  color.blue = color.withRgb(0, 0, 1)
-  color.yellow = color.withRgb(1, 1, 0)
-  color.cyan = color.withRgb(0, 1, 1)
-  color.magenta = color.withRgb(1, 0, 1)
+color.white = color.withRgb(1, 1, 1)
+color.black = color.withRgb(0, 0, 0)
+color.red = color.withRgb(1, 0, 0)
+color.green = color.withRgb(0, 1, 0)
+color.blue = color.withRgb(0, 0, 1)
+color.yellow = color.withRgb(1, 1, 0)
+color.cyan = color.withRgb(0, 1, 1)
+color.magenta = color.withRgb(1, 0, 1)
 
-  return color
+return color
--- rotate_screen.lua
+++ rotate_screen.lua
@@ -1,13 +1,13 @@
 local rotateScreen = {}
 function rotateScreen:new(w, h)
-  return setmetatable({w=w, h=h}, {__index=self})
+  return setmetatable({ w = w, h = h }, { __index = self })
 end
 
 function rotateScreen:beginDraw(r)
   love.graphics.push()
-  love.graphics.translate(self.w/2, self.h/2)
-	love.graphics.rotate(r)
-  love.graphics.translate(-self.h/2, -self.w/2)
+  love.graphics.translate(self.w / 2, self.h / 2)
+  love.graphics.rotate(r)
+  love.graphics.translate(-self.h / 2, -self.w / 2)
 end
 
 function rotateScreen:endDraw()
